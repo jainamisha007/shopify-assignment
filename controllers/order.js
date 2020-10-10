f1ab87f4ffbca7be8b7d159af53aba184966cc93
thegirlnotknownn <jainamisha.j@gmail.com>
2020-10-10 18:55:24 +0530

    ext

--- controllers/order.js
+++ controllers/order.js
@@ -8,6 +8,7 @@ exports.getOrders = (req, res) => {
     method: 'GET',
     url: `https://${apikey}:${password}@samplestoreamisha.myshopify.com/admin/api/2020-10/orders.json`
   }).then((response) => {
+    console.log(response.data.orders);
     return res.render('orders', {
       title: "Orders",
       data: response.data.orders
